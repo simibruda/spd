10046c61b6874fea5f8d17175243c4182a175649
unknown <simi.booy@yahoo.com>
2022-10-13 16:29:23 +0300

    navv

---
+++ proiectspd/src/components/Navbar.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div className='menu'>
+      <nav id='menu'>
+  
+  <ul>
+    <li><a href='http://'>Home</a></li>
+    <li><a href='http://'>Dashboard</a>
+     
+    </li>
+    <li><a href='http://'>Infografic</a></li>
+  </ul>
+</nav>
+    </div>
+  )
+}
+
+export default Navbar
